331f1bee0215a452ce2a400457c5375efcfd7a57
Ivan Smirnov <i.s.smirnov@gmail.com>
2013-02-22 02:00:01 +0400

    -

--- raytest.py
+++ raytest.py
@@ -5,7 +5,7 @@ import time
 
 if __name__ == '__main__':
     rayeval.load_handranks('HandRanks.dat')
-    rayeval.load_handranks_9('hr9.dat')
+    # rayeval.load_handranks_9('hr9.dat')
 
     game = 'omaha'
     board = '2h 2d 2s * *'
@@ -21,13 +21,13 @@ if __name__ == '__main__':
     print 'Elapsed: %.2f seconds (%.2fM iterations / sec).' % (
         elapsed, iterations / elapsed / 1e6)
 
-    t0 = time.time()
-    ev = rayeval.eval_mc('omaha_9', board, pocket, iterations, n_jobs)
-    elapsed = time.time() - t0
-    print '[%s]' % game, pocket[:1], 'vs', pocket[1:], (
-        ': EV = %.4f%% (%.2gM iterations).' % (100. * ev[0], iterations / 1e6))
-    print 'Elapsed: %.2f seconds (%.2fM iterations / sec).' % (
-        elapsed, iterations / elapsed / 1e6)
+    # t0 = time.time()
+    # ev = rayeval.eval_mc('omaha_9', board, pocket, iterations, n_jobs)
+    # elapsed = time.time() - t0
+    # print '[%s]' % game, pocket[:1], 'vs', pocket[1:], (
+    #     ': EV = %.4f%% (%.2gM iterations).' % (100. * ev[0], iterations / 1e6))
+    # print 'Elapsed: %.2f seconds (%.2fM iterations / sec).' % (
+    #     elapsed, iterations / elapsed / 1e6)
 
     import pokereval
     t0 = time.time()
@@ -35,5 +35,19 @@ if __name__ == '__main__':
         pockets=[['Kh', '9h', 'Ks', '9s'], ['__', '__', '__', '__'],
         ['__', '__', '__', '__'], ['__', '__', '__', '__']], iterations=int(1e6))
     elapsed = time.time() - t0
+    print 'Elapsed: %.2f seconds (%.2fM iterations / sec).' % (
+        elapsed, iterations / elapsed / 1e6)
+
+    game = 'holdem'
+    board = '* * * * *'
+    pocket = ['Kh 9h', 'As Ac']
+    iterations = 1e6
+    n_jobs = 8
+
+    t0 = time.time()
+    ev = rayeval.eval_mc(game, board, pocket, iterations, n_jobs)
+    elapsed = time.time() - t0
+    print '[%s]' % game, pocket[:1], 'vs', pocket[1:], (
+        ': EV = %.4f%% (%.2gM iterations).' % (100. * ev[0], iterations / 1e6))
     print 'Elapsed: %.2f seconds (%.2fM iterations / sec).' % (
         elapsed, iterations / elapsed / 1e6)
