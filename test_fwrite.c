daa748575aa8e31b24bed8218f878bdc471d87ac
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2013-02-23 02:31:23 +0400

    test_fwrite.c: updated test, using load_file

--- test_fwrite.c
+++ test_fwrite.c
@@ -3,7 +3,27 @@
 #include <string.h>
 
 #define HR9_SIZE 	    620518328 // 134217728
-#define READ_BLOCK_SIZE 500000000
+#define READ_BLOCK_SIZE 2000000000 
+
+int load_file(void* dest, size_t size, size_t nitems, FILE* stream)
+{
+    int ret = 0;
+    int read_block_nitems = READ_BLOCK_SIZE / size;
+    int nitems_block;
+    int offset = 0;
+    int nitems_left = nitems;
+        
+    while (nitems_left > 0)
+    {        
+        nitems_block = nitems_left > read_block_nitems ? read_block_nitems : nitems_left;
+        ret += fread(dest+offset, size, nitems_block, stream);
+        offset += nitems_block * size;
+        nitems_left -= nitems_block;
+    }
+    
+    return ret;
+}
+
 
 
 int main()
@@ -31,10 +51,9 @@ int main()
     
 	f = fopen("test_fwrite.dat", "rb");
     int ret = 0;
-	
+    int nret = 0;
     HR9 = (int *) malloc(HR9_SIZE * sizeof(int));
-    ret = fread(HR9, sizeof(int), READ_BLOCK_SIZE, f);
-    ret += fread(HR9 + READ_BLOCK_SIZE, sizeof(int), HR9_SIZE - READ_BLOCK_SIZE, f);
+    ret = load_file(HR9, sizeof(int), HR9_SIZE, f);
 	fclose(f);
     
     printf("\nret: %d\n", ret);
